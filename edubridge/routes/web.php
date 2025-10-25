90172ebc04648e2b8678b0a571b1693eb089b032
Yasithb <pybpeiris@std.foc.sab.ac.lk>
2025-10-26 00:14:09 +0530

    Update web.php

--- edubridge/routes/web.php
+++ edubridge/routes/web.php
@@ -9,3 +9,15 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('auth.login');
 });
+
+Route::get('/teacher/dashboard', function () {
+    return view('teacher.dashboard');
+});
+
+Route::get('/teacher/classes', function () {
+    return view('teacher.classes.index');
+});
+
+Route::get('/teacher/classes/students', function () {
+    return view('teacher.classes.students');
+});
